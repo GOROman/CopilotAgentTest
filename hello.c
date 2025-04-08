3af7407229e49f3f427a5d29ae7af82d9486e722
GOROman <yoshihito.kondoh@gmail.com>
2025-04-08 16:08:33 +0900

    初回コミット: Hello Worldプログラムを追加

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main関数 - エントリーポイント
+ * 
+ * @return 正常終了の場合は0を返す
+ */
+int main() {
+    // 標準出力に"Hello, World!"を表示する
+    printf("Hello, World!\n");
+    return 0;
+}
